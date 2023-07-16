8e3e297c65fe1e3a11ac4b9b06b19d642e1f6970
Retsam <chess19@gmail.com>
2023-07-16 13:41:05 -0400

    Basic program structure

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
+import java.io.InputStreamReader
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+  if (args.size > 1) {
+    println("Usage: klox [script]")
+    exitProcess(64)
+  } else if (args.size == 1) {
+    runFile(args[0])
+  } else {
+    runPrompt()
+  }
+}
+
+fun runFile(path: String) {
+  val bytes: ByteArray = File(path).readBytes()
+  run(String(bytes, Charsets.UTF_8))
+}
+
+fun runPrompt() {
+  val reader = InputStreamReader(System.`in`)
+  val bufferedReader = reader.buffered()
+  while (true) {
+    print("> ")
+    val line = bufferedReader.readLine() ?: break
+    run(line)
+  }
+}
+
+fun run(code: String) {
+  println("Should execute `$code`")
 }
