649d1f0ec844ac2572173a3aa11cad509acd4ed6
Retsam <chess19@gmail.com>
2023-07-24 21:34:59 -0400

    Add Expr classes

---
+++ src/main/kotlin/Expr.kt
@@ -0,0 +1,9 @@
+sealed interface Expr
+
+class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr
+
+class Grouping(val expression: Expr) : Expr
+
+class Literal(val value: Any?) : Expr
+
+class Unary(val operator: Token, val right: Expr) : Expr
